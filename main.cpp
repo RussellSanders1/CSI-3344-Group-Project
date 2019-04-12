27ae85eaa5df01832dfa270e2c7f781bfc3a66d5
Russell Sanders <russellsanders121@gmail.com>
2019-04-11 21:33:15 -0500

    Fixed SDL errors (I think)

--- main.cpp
+++ main.cpp
@@ -1,44 +1,42 @@
-// A C++ program to find convex hull of a set of points. Refer
-// https://www.geeksforgeeks.org/orientation-3-ordered-points/
-// for explanation of orientation()
 #include <iostream>
 #include <stack>
 #include <stdlib.h>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include "point.h"
+#include "SDL_Plotter.h"
 using namespace std;
-
-struct Point
-{
-    int x, y;
-};
 
 // A global point needed for sorting points with reference
 // to the first point Used in compare function of qsort()
-Point p0;
+point p0;
 
 // A utility function to find next to top in a stack
-Point nextToTop(stack<Point> &S)
+point nextToTop(stack<point> &S)
 {
-    Point p = S.top();
+    point p = S.top();
     S.pop();
-    Point res = S.top();
+    point res = S.top();
     S.push(p);
     return res;
 }
 
 // A utility function to swap two points
-int swap(Point &p1, Point &p2)
+void swap(point &p1, point &p2)
 {
-    Point temp = p1;
+    point temp = p1;
     p1 = p2;
     p2 = temp;
 }
 
 // A utility function to return square of distance
 // between p1 and p2
-int distSq(Point p1, Point p2)
+int distSq(point p1, point p2)
 {
-    return (p1.x - p2.x)*(p1.x - p2.x) +
-           (p1.y - p2.y)*(p1.y - p2.y);
+    return (p1.getX() - p2.getX())*(p1.getX() - p2.getX()) +
+           (p1.getY() - p2.getY())*(p1.getY() - p2.getY());
 }
 
 // To find orientation of ordered triplet (p, q, r).
@@ -46,10 +44,10 @@ int distSq(Point p1, Point p2)
 // 0 --> p, q and r are colinear
 // 1 --> Clockwise
 // 2 --> Counterclockwise
-int orientation(Point p, Point q, Point r)
+int orientation(point p, point q, point r)
 {
-    int val = (q.y - p.y) * (r.x - q.x) -
-              (q.x - p.x) * (r.y - q.y);
+    int val = (q.getY() - p.getY()) * (r.getX() - q.getX()) -
+              (q.getX() - p.getX()) * (r.getY() - q.getY());
     
     if (val == 0) return 0; // colinear
     return (val > 0)? 1: 2; // clock or counterclock wise
@@ -57,44 +55,34 @@ int orientation(Point p, Point q, Point r)
 
 // A function used by library function qsort() to sort an array of
 // points with respect to the first point
-int compare(const void *vp1, const void *vp2)
-{
-    Point *p1 = (Point *)vp1;
-    Point *p2 = (Point *)vp2;
+int compare(const point &p1, const point &p2){
 
 // Find orientation
-    int o = orientation(p0, *p1, *p2);
+    int o = orientation(p0, p1, p2);
     if (o == 0)
-        return (distSq(p0, *p2) >= distSq(p0, *p1))? -1 : 1;
+        return (distSq(p0, p2) >= distSq(p0, p1))? -1 : 1;
     
     return (o == 2)? -1: 1;
 }
 
 // Prints convex hull of a set of n points.
-void convexHull(Point points[], int n)
-{
+void convexHull(set<point> &points, int n){
+    
+    priority_queue<point, set<point>, decltype(compare)> queue(compare, points);
+    
 // Find the bottommost point
-    int ymin = points[0].y, min = 0;
-    for (int i = 1; i < n; i++)
-    {
-        int y = points[i].y;
-        
-        // Pick the bottom-most or chose the left
-        // most point in case of tie
-        if ((y < ymin) || (ymin == y &&
-                           points[i].x < points[min].x))
-            ymin = points[i].y, min = i;
-    }
+    //int ymin = queue.top().getY();
 
 // Place the bottom-most point at first position
-    swap(points[0], points[min]);
+    //swap(points[0], points[min]);
 
 // Sort n-1 points with respect to the first point.
 // A point p1 comes before p2 in sorted ouput if p2
 // has larger polar angle (in counterclockwise
 // direction) than p1
-    p0 = points[0];
-    qsort(&points[1], n-1, sizeof(Point), compare);
+    p0 = queue.top();
+    //sort(++points.begin(), points.end(), compare);
+    //qsort(&points[1], n-1, sizeof(point), compare);
 
 // If two or more points make same angle with p0,
 // Remove all but the one that is farthest from p0
@@ -106,10 +94,7 @@ void convexHull(Point points[], int n)
     {
         // Keep removing i while angle of i and i+1 is same
         // with respect to p0
-        while (i < n-1 && orientation(p0, points[i],
-                                      points[i+1]) == 0)
-            i++;
-        
+        while (i < n-1 && orientation(p0, points[i], points[i+1]) == 0) i++;
         
         points[m] = points[i];
         m++; // Update size of modified array
@@ -121,7 +106,7 @@ void convexHull(Point points[], int n)
 
 // Create an empty stack and push first three points
 // to it.
-    stack<Point> S;
+    stack<point> S;
     S.push(points[0]);
     S.push(points[1]);
     S.push(points[2]);
@@ -140,18 +125,19 @@ void convexHull(Point points[], int n)
 // Now stack has the output points, print contents of stack
     while (!S.empty())
     {
-        Point p = S.top();
-        cout << "(" << p.x << ", " << p.y <<")" << endl;
+        point p = S.top();
+        cout << "(" << p.getX() << ", " << p.getY() <<")" << endl;
         S.pop();
     }
 }
 
 // Driver program to test above functions
-int main()
-{
-    Point points[] = {{0, 3}, {1, 1}, {2, 2}, {4, 4},
-                      {0, 0}, {1, 2}, {3, 1}, {3, 3}};
-    int n = sizeof(points)/sizeof(points[0]);
+int main(int argc, char** argv){
+    SDL_Plotter g(500,1000);
+    
+    set<point> points = {point(0,3), point(1,1), point(2,2), point(4,4),
+                            point(0,0), point(1,2), point(3,1), point(3,3)};
+    int n = points.size();
     convexHull(points, n);
     return 0;
 }
