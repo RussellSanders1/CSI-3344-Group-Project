531f0180a2875563201c4b71715064bc9020fb75
Zachary Kearney <zacharykearney@baylor.edu>
2019-04-27 16:02:01 -0500

    ZCK - Finished D&C closest pair, made improvement on brute force

--- closest_pair.cpp
+++ closest_pair.cpp
@@ -3,14 +3,20 @@
 #include <cmath>
 #include <algorithm>
 #include <iterator>
+#include <cassert>
+
 using namespace std;
 
 struct Point{
     double x, y;
 };
 
-bool compare(const Point &a, const Point &b){
+bool compareX(const Point &a, const Point &b){
     return a.x < b.x;
+}
+
+bool compareY(const Point &a, const Point &b){
+    return a.y < b.y;
 }
 
 double distance(const Point &p1, const Point &p2){
@@ -23,6 +29,7 @@ double distance(const pair<Point,Point> p){
 
 pair<Point,Point> smallestDistance(const pair<Point,Point> a, const pair<Point, Point> b){
     pair<Point,Point> closest;
+    assert(&a!=&b);
     if(distance(a) <= distance(b)){
         closest = a;
     }
@@ -32,14 +39,33 @@ pair<Point,Point> smallestDistance(const pair<Point,Point> a, const pair<Point, 
     return closest;
 }
 
+pair<Point,Point> stripClosestPair(vector<Point> strip){
+    assert(strip.size() > 1);
+    double min = numeric_limits<double>::max();
+    sort(strip.begin(),strip.end(),compareY);
+    Point a = strip[0];
+    Point b = strip[strip.size()-1];
+    for(int i = 0; i < strip.size(); i++){
+        for(int j = i + 1; j < strip.size() && (strip[j].y - strip[i].y) < min; j++){
+            if(&strip[i] != &strip[j] && distance(strip[i],strip[j]) < min){
+                min = distance(strip[i],strip[j]);
+                a = strip[i];
+                b = strip[j];
+            }
+        }
+    }
+    return make_pair(a,b);
+}
 
-pair<Point,Point> bruteForce(vector<Point> points){
+pair<Point,Point> bruteForce(vector<Point> points,int begin,int end){
     double closestDist = INT_MAX;
-    Point p1,p2;
-    for(int i = 0; i < points.size(); i++){
-        for(int j = 0; j <points.size(); j++){
+    Point p1 = points[begin];
+    Point p2 = points[end];
+    assert(end - begin > 0);
+    for(int i = begin; i < end; i++){
+        for(int j = begin; j < end; j++){
             if(&points[i] != &points[j]){
-                double dist = sqrt(pow(points[j].x-points[i].x,2)+pow(points[j].y-points[i].y,2));
+                double dist = distance(points[i],points[j]);
                 if(dist < closestDist){
                     closestDist = dist;
                     p1 = points[i];
@@ -55,32 +81,46 @@ pair<Point,Point> divideAndConquer(vector<Point> points, int begin, int end, boo
     // Brute force at most 3 points
     int size = end - begin;
     if(size <= 3){
-        return bruteForce(points);
+        return bruteForce(points,begin,end);
     }
 
-    int mid = size/2;
+    int mid = size/2 + begin;
     Point midpoint;
     midpoint.x = points[mid].x;
     midpoint.y = points[mid].y;
 
     pair<Point, Point> left = divideAndConquer(points,begin,mid,false);
-    pair<Point, Point> right = divideAndConquer(points,mid + 1,end,false);
-    double dist = min(distance(left),distance(right));
+    pair<Point, Point> right = divideAndConquer(points,mid+1,end,false);
+    pair<Point, Point> closest = smallestDistance(left,right);
+    double dist = distance(closest);
+
+    // Make strip
+    vector<Point> strip;
+    for(int i = begin; i < end; i++){
+        if(abs(points[i].x - midpoint.x) < dist){
+            strip.push_back(points[i]);
+        }
+    }
+
+    // Get the closest pair in the strip.
+    if(strip.size() > 1){
+        pair<Point,Point> stripClosest = stripClosestPair(strip);
+        // Compare cloest pair in the strip with the previous closest.
+        closest = smallestDistance(closest,stripClosest);
+    }
+
     if(display){
         // Draw shortest pairs on left and right
+
     }
 
 
-
-
+    return closest;
 }
 
 
-
-
-
 int main() {
-    freopen("DataPoints1k.txt","r",stdin);
+    freopen("CPDataPoints10k.txt","r",stdin);
     int number;
     cin >> number;
     vector<Point> points;
@@ -90,17 +130,10 @@ int main() {
         points.push_back(p);
     }
 
-    sort(points.begin(),points.end(),compare);
+    sort(points.begin(), points.end(), compareX);
     auto it = &points[points.size()/2];
 
-
-    /*
-    for(auto it = points.begin(); it != points.end(); it++){
-        cout << "(" << it->x << "," << it->y << ")" << endl;
-    }
-    */
-
-    pair<Point,Point> closestPair = bruteForce(points);
+    pair<Point,Point> closestPair = bruteForce(points,0,points.size());
     Point p1 = closestPair.first;
     Point p2 = closestPair.second;
     double closestDist = sqrt(pow(p2.x-p1.x,2)+pow(p2.y-p1.y,2));
@@ -108,7 +141,17 @@ int main() {
     cout << "Points:" << endl;
     cout << "(" << p1.x << "," << p1.y << ")" << endl;
     cout << "(" << p2.x << "," << p2.y << ")" << endl;
+    cout << "Distance: " << closestDist << endl << endl;
+
+    closestPair = divideAndConquer(points,0,points.size(),false);
+    p1 = closestPair.first;
+    p2 = closestPair.second;
+    closestDist = distance(closestPair);
+    cout << "Points:" << endl;
+    cout << "(" << p1.x << "," << p1.y << ")" << endl;
+    cout << "(" << p2.x << "," << p2.y << ")" << endl;
     cout << "Distance: " << closestDist << endl;
+
 
     return 0;
 }
